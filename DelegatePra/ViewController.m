707c06d681db336ea520c88e147ab218cbb89258
Ling <asklingtoby@gmail.com>
2016-06-29 06:29:32 -0400

    Method1 - delegate +refer containView to ChildVC, delegate assigned at button press

--- DelegatePra/ViewController.m
+++ DelegatePra/ViewController.m
@@ -33,14 +33,20 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+//Method1 - delegate +refer containView to ChildVC, delegate assign at button pressed
 - (IBAction)updateMapView:(UIButton *)sender {
+    
+        MapViewController *mvc = self.childViewControllers[0];
+        [self setDelegate:mvc];
+    
     NSLog(@"button pressed, waitting to Log in container view");
     [self.delegate changeMaker];
 }
 
 //Method2 - delegate +refer containView to segue destinationViewController
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender{
-    MapViewController *mvc = [segue destinationViewController];
-    [self setDelegate:mvc];
+//    MapViewController *mvc = [segue destinationViewController];
+//    [self setDelegate:mvc];
 }
 @end
